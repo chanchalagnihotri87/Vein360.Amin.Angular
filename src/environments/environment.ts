832b57715a7b0468f6e1c51fd8183620f5c8639a
chanchalagnihotri87 <74895265+chanchalagnihotri87@users.noreply.github.com>
2026-01-14 18:42:38 +0530

    Update API URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'https://vein-360-api-avgccdhghpahehgj.eastus2-01.azurewebsites.net',
+  apiUrl: 'https://vein360-api-dev-auekcfcdfkdggace.canadacentral-01.azurewebsites.net',
 };
